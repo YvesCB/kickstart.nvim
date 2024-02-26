0b615d2d20ac9bd02bd7b927e926730e4c0a9707
YvesCB <y.bruckmayer@gmail.com>
2024-02-26 13:46:47 +0100

    removed rust tools and debug

--- after/plugin/rust-tools.lua
+++ after/plugin/rust-tools.lua
@@ -1,71 +1,71 @@
-local rt = require("rust-tools")
-
--- Update this path
-local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0-universal/'
-local codelldb_path = extension_path .. 'adapter/codelldb'
-local liblldb_path = extension_path .. 'lldb/lib/liblldb'
-local this_os = vim.loop.os_uname().sysname;
-
--- The path in windows is different
-if this_os:find "Windows_NT" then
-  codelldb_path = extension_path .. "adapter\\codelldb.exe"
-  liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-else
-  -- The liblldb extension is .so for linux and .dylib for macOS
-  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-end
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = 'LSP: ' .. desc
-        end
-
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-      end
-      -- Hover actions
-      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-      nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-      nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-      nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-      nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-      -- See `:help K` for why this keymap
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-      -- Lesser used LSP functionality
-      nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[W]orkspace [L]ist Folders')
-
-      -- Create a command `:Format` local to the LSP buffer
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-      end, { desc = 'Format current buffer with LSP' })
-    end,
-  },
-  tools = {
-    inlay_hints = {
-      auto = false,
-    },
-  },
-  dap = {
-    adapter = require('rust-tools.dap').get_codelldb_adapter(
-      codelldb_path, liblldb_path)
-  }
-})
+-- local rt = require("rust-tools")
+--
+-- -- Update this path
+-- local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0-universal/'
+-- local codelldb_path = extension_path .. 'adapter/codelldb'
+-- local liblldb_path = extension_path .. 'lldb/lib/liblldb'
+-- local this_os = vim.loop.os_uname().sysname;
+--
+-- -- The path in windows is different
+-- if this_os:find "Windows_NT" then
+--   codelldb_path = extension_path .. "adapter\\codelldb.exe"
+--   liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+-- else
+--   -- The liblldb extension is .so for linux and .dylib for macOS
+--   liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+-- end
+--
+-- rt.setup({
+--   server = {
+--     on_attach = function(_, bufnr)
+--       -- Hover actions
+--       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+--       -- Code action groups
+--       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+--
+--       local nmap = function(keys, func, desc)
+--         if desc then
+--           desc = 'LSP: ' .. desc
+--         end
+--
+--         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+--       end
+--       -- Hover actions
+--       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+--       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+--
+--       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+--       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+--       nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+--       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+--       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+--       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+--
+--       -- See `:help K` for why this keymap
+--       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+--       nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+--
+--       -- Lesser used LSP functionality
+--       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+--       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+--       nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+--       nmap('<leader>wl', function()
+--         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--       end, '[W]orkspace [L]ist Folders')
+--
+--       -- Create a command `:Format` local to the LSP buffer
+--       vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+--         vim.lsp.buf.format()
+--       end, { desc = 'Format current buffer with LSP' })
+--     end,
+--   },
+--   tools = {
+--     inlay_hints = {
+--       auto = false,
+--     },
+--   },
+--   dap = {
+--     adapter = require('rust-tools.dap').get_codelldb_adapter(
+--       codelldb_path, liblldb_path)
+--   }
+-- })
